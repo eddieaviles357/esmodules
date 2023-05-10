66c1627ea52088954d4e787e2d6d5b782923c010
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-09 19:16:33 -0700

    removed console logs

--- src/index.js
+++ src/index.js
@@ -5,11 +5,7 @@ import { choice, remove } from './helpers';
 
 const App = () => {
   const fruit = choice(fruits);
-  console.log(`I'll like one ${fruit}, please!`);
-  console.log(`Here you go: ${fruit}`)
-  console.log(`Delicious! May I have another?`);
   remove(fruits, fruit);
-  console.log(`I’m sorry, we’re all out. We have ${fruits.length} left.`)
   return (
     <div>
       <p>I'll like one {fruit}, please!</p>
